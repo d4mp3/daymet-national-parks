1e28d610dac7d673557ceb7e740769a103c3cfe8
d4mp3 <damian.pszczola@gmail.com>
2024-06-12 01:13:44 +0200

    add hadoop-base conf files

--- docker/hadoop/hadoop-datanode/start-datanode.sh
+++ docker/hadoop/hadoop-datanode/start-datanode.sh
@@ -1,20 +1,10 @@
 #!/bin/bash
 
-NAME_DIR=`echo $HDFS_CONF_DFS_NAMENODE_DATA_DIR | perl -pe 's#file://##'`
+DATA_DIR=`echo $HDFS_CONF_DFS_DATANODE_DATA_DIR | perl -pe 's#file://##'`
 
-if [ ! -d $NAME_DIR ]; then
-    echo "Namenode name directory not found: $NAME_DIR"
+if [ ! -d $DATA_DIR ]; then
+    echo "Datanode data directory not found: $DATA_DIR"
     exit 2
 fi
 
-if [ -z "$CLUSTER_NAME" ]; then
-    echo "Cluster name not specified"
-    exit 2
-fi
-
-if [ "`ls -A $NAME_DIR`" == "" ]; then
-    echo "Formatting namenode directory: $NAME_DIR"
-    $HADOOP_HOME/bin/hdfs --config $HADOOP_CONF_DIR namenode -format $CLUSTER_NAME
-fi
-
-$HADOOP_HOME/bin/hdfs --config $HADOOP_CONF_DIR namenode
+$HADOOP_HOME/bin/hdfs --config $HADOOP_CONF_DIR datanode
