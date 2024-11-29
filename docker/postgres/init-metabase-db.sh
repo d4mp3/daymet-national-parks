f0817610bbeffd4ea105e6118011c3f18822ad24
d4mp3 <damian.pszczola@gmail.com>
2024-11-29 02:51:37 +0100

    set metabase to prod mode

--- docker/postgres/init-metabase-db.sh
+++ docker/postgres/init-metabase-db.sh
@@ -5,14 +5,7 @@ psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-E
   DROP DATABASE IF EXISTS metabase;
   DROP ROLE IF EXISTS metabase;
   CREATE USER metabase WITH PASSWORD 'metabase';
+  GRANT CREATE ON SCHEMA public TO metabase;
+  ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT UPDATE, INSERT, SELECT, DELETE, CREATE ON TABLES TO metabase;
   CREATE DATABASE metabase;
-  GRANT ALL PRIVILEGES ON DATABASE metabase TO metabase;
-  \c metabase
-  \t
-  \o /tmp/grant-privs
-SELECT 'GRANT SELECT,INSERT,UPDATE,DELETE ON "' || schemaname || '"."' || tablename || '" TO metabase ;'
-FROM pg_tables
-WHERE tableowner = CURRENT_USER and schemaname = 'public';
-  \o
-  \i /tmp/grant-privs
-EOSQL
+  ALTER DATABASE metabase owner to metabase;
