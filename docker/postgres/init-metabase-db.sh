89192466ae0bf58c30a2be821ec216539ae91367
d4mp3 <damian.pszczola@gmail.com>
2025-03-01 23:40:19 +0100

    use .env pass in metabase db initialization

--- docker/postgres/init-metabase-db.sh
+++ docker/postgres/init-metabase-db.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 set -e
 
+if [ -f ../../.env ]; then
+  source .env
+else
+  echo ".env file not found!"
+  exit 1
+fi
+
 echo "Checking if Metabase database exists..."
 DB_EXISTS=$(psql -U "$POSTGRES_USER" -d "$POSTGRES_DB" -tAc "SELECT 1 FROM pg_database WHERE datname='metabase'")
 
@@ -9,10 +16,12 @@ if [ "$DB_EXISTS" = "1" ]; then
 else
   echo "Creating Metabase database..."
   psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
-    CREATE USER metabase WITH PASSWORD 'metabase';
+    CREATE USER metabase WITH PASSWORD '${MB_DB_PASS}';
     CREATE DATABASE metabase;
     ALTER DATABASE metabase OWNER TO metabase;
     GRANT ALL PRIVILEGES ON DATABASE metabase TO metabase;
 EOSQL
   echo "Metabase database created successfully."
 fi
+
+unset PGPASSWORD
